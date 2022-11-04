adc09f12630f527a32eafdc4f87ac278f2239f83
Mriganka Gogoi <gogoimriganka09@gmail.com>
2022-11-04 13:22:38 +0530

    Javascript Day 2

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,91 @@
+/*
+Values and variables
+const country = "India";
+const continent = "Asia";
+let population = 1500;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+/*
+// Data Types
+let rollNo = 5137;
+let javascriptIsFun = true;
+let firstName = "Mriganka";
+
+console.log(rollNo);
+console.log(typeof rollNo);
+
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+console.log(firstName);
+console.log(typeof firstName);
+
+//Dynamically typed
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+// Undefined
+let done;
+console.log(done);
+console.log(typeof done);
+
+// BUG IN JS
+let bug = null;
+console.log(typeof bug); // It outputs typeof null as OBJECT TYPE
+*/
+
+/*
+
+//Assignment Data Types
+const isIsland = false;
+let language = "Assamese";
+
+console.log(typeof isIsland);
+console.log(typeof language);
+
+// Math Operators
+const now = 2022;
+const myAge = now - 1998;
+const herAge = now - 1999;
+
+console.log(myAge);
+console.log(herAge);
+
+// Assignment Opeartors
+let x = 12;
+x = 15;
+x += 10;
+x++;
+x *= 4;
+x--;
+console.log(x);
+
+// Comparasion Opeartor
+console.log(myAge > herAge);
+console.log(myAge >= herAge * 2);
+
+Basic OperatorsAssignment
+console.log(population / 2 + " Millions");
+population += 1;
+console.log(population);
+
+const populationFinland = 6;
+console.log(population > populationFinland);
+console.log(population < 33);
+*/
+/*
+let description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,8 @@
+/*
 let js = "Awesome";
 if (js === "Awesome") {
   alert("JavaScript is FUN!");
 }
 
 console.log(5 + 7 + 42 - 89 * 321);
+*/
